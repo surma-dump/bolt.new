311ea9b1153913a9c6d3a13b34d0dcd0f918e5b0
Surma <surma@surma.dev>
2024-11-08 09:38:05 +0000

    Allow OpenAI

--- app/lib/.server/llm/model.ts
+++ app/lib/.server/llm/model.ts
@@ -1,9 +1,17 @@
 import { createAnthropic } from '@ai-sdk/anthropic';
+import {createOpenAI} from '@ai-sdk/openai';
+import * as fs from "node:fs";
 
 export function getAnthropicModel(apiKey: string) {
-  const anthropic = createAnthropic({
+  // const anthropic = createAnthropic({
+  //   apiKey,
+  // });
+  // return anthropic('claude-3-5-sonnet-20240620');
+
+  const model = createOpenAI({
+    baseURL: "https://proxy.shopify.ai/v1",
     apiKey,
   });
 
-  return anthropic('claude-3-5-sonnet-20240620');
+  return model('anthropic:claude-3-5-sonnet-20240620');
 }
